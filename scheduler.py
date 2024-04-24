1c620d26e0c232bdf89587481117d2a86bbfce15
Nick Rodriguez <nrodrig1@gmail.com>
2024-04-24 14:33:16 +0200

    feat: added 3 new tasks to run daily to open and close a db_session with wrap_up_session()

--- scheduler.py
+++ scheduler.py
@@ -22,6 +22,13 @@ def scheduler_initiator():
     # job_ws_weather_and_UserLocationDay_updater = scheduler.add_job(scheduler_manager, 'cron', hour='*', minute='47', second='25')#Testing
     # job_call_harmless = scheduler.add_job(harmless, 'cron',  hour='*', minute='03', second='35')#Testing
 
+    daily_db_session_closer_0h = scheduler.add_job(create_and_close_db_session, 'cron', day='*', hour='00', minute='00', second='00')#Production
+    daily_db_session_closer_8h = scheduler.add_job(create_and_close_db_session, 'cron', day='*', hour='08', minute='00', second='00')#Production
+    daily_db_session_closer_16h = scheduler.add_job(create_and_close_db_session, 'cron', day='*', hour='16', minute='00', second='00')#Production
+
+    # daily_db_session_closer_tester = scheduler.add_job(create_and_close_db_session, 'cron', day='*', hour='14', minute='29', second='25')#Testing
+
+
     scheduler.start()
 
     while True:
@@ -38,41 +45,20 @@ def harmless():
 
 def scheduler_manager():
 
-    # logger_scheduler.info("Start Interpolation")
-    # interpolate_UserLocationDay_manager()
-
     logger_scheduler.info("Start Send Visual Crossing API weather reports")
-    update_weather_history()
+    collect_yesterday_weather_history_from_visual_crossing()
     logger_scheduler.info("--- ENDED What Sticks 11 Scheduler ---")
 
-# # OBE due to ws_analysis/ create_df_daily_user_location_consecutive
-# def interpolate_UserLocationDay_manager():
-#     db_session = DatabaseSession()
-#     users_list = db_session.query(Users).all()
-
-#     for user in users_list:
-#         query = db_session.query(UserLocationDay).filter(UserLocationDay.user_id == user.id)
-#         # # Convert the query result to a list of dictionaries
-#         df_existing_user_locations = pd.read_sql(query.statement, db_session.bind)
-
-#         if len(df_existing_user_locations) > 1:
-#             interpolated_df = interpolate_missing_dates_exclude_references(df_existing_user_locations)
-
-#             logger_scheduler.info(f"interpolated_df Length: {len(interpolated_df)}")
-
-#             if len(interpolated_df) > 0:
-#                 interpolated_df.to_sql('user_location_day', con=engine, if_exists='append', index=False)
-    
-#     wrap_up_session(db_session)
+def create_and_close_db_session():
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+    logger_scheduler.info(f"- in create_and_close_db_session [timestamp:{timestamp}] ")
+    db_session = DatabaseSession()
+    logger_scheduler.info(f"- db_session ID: {id(db_session)} [timestamp:{timestamp}]")
+    wrap_up_session(db_session)
+    logger_scheduler.info(f"- END create_and_close_db_session [timestamp:{timestamp}]")
 
 
-def update_weather_history():
-    collect_yesterday_weather_history_from_visual_crossing()
-    # ###########################################################################################
-    # # This function updates the weather history for yesterday for all locations in Locations ##
-    # ###########################################################################################  
 
-    logger_scheduler.info(f"- Finished update_weather_history from WS11Scheduler ")
 
 if __name__ == '__main__':  
     scheduler_initiator()
